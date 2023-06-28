51adeae71064250859a3a0a144d23eb08dc7060c
Julian Gonzalez <julian.gonzalez@geniussports.com>
2023-06-28 16:08:34 -0500

    Add showcase content to render local video

---
+++ showcase-ios/showCase/showCaseApp.swift
@@ -0,0 +1,18 @@
+//
+//  showCaseApp.swift
+//  showCase
+//
+//  Created by juan castro on 7/05/23.
+//
+
+import SwiftUI
+
+@main
+struct showCaseApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    var body: some Scene {
+      WindowGroup {
+        ContentView()
+      }
+    }
+}
